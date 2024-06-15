9705877c3793b3aa300c4829008e54e60de1964e
arrorobbi <vickyrobbi@gmail.com>
2024-06-15 17:35:25 +0700

    fix: auth login

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from '../app.controller';
 import { AppService } from '../app.service';
 import { UserModule } from 'src/modules/user.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { sequelizeConfig } from '../sequelize.config';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    SequelizeModule.forRoot(sequelizeConfig), //use sequelize config
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/user.module.ts
+++ src/modules/user.module.ts
@@ -3,19 +3,17 @@ import { UserController } from 'src/user/user.controller';
 import { UserService } from 'src/user/user.service';
 import { JwtGuard } from 'src/middlewares/auth-jwt';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { sequelizeConfig } from '../sequelize.config';
 import { User } from '../models/user';
 import { Address } from '../models/address';
+import { AuthController } from 'src/auth/auth.controller';
 
 @Module({
-  imports: [
-    SequelizeModule.forRoot(sequelizeConfig), //use sequelize config
-    SequelizeModule.forFeature([User, Address]),
-  ],
-  controllers: [UserController],
-  providers: [UserService],
+  imports: [SequelizeModule.forFeature([User, Address])], //import model
+  controllers: [UserController, AuthController], // import controller that needed in this module
+  providers: [UserService], // import services that used in controller
 })
 export class UserModule {
+  // set middleware
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(JwtGuard)
